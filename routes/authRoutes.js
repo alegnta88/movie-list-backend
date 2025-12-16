a6491c8098c3cbefd79468c5b6fb7c86ddf7051e
alegnta88 <alegnta88@gmail.com>
2025-12-16 14:52:59 +0300

    add routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { userLogin, userRegister, getAllUsers, userLogout, verifyOTP } from '../controllers/authController.js';
+import { userLogin, userRegister, getAllUsers, userLogout, verifyOTP, resendOTP } from '../controllers/authController.js';
 
 const authRoutes = express.Router();
 
 authRoutes.post('/register', userRegister);
 authRoutes.post('/verify', verifyOTP);
+authRoutes.post('/resend', resendOTP);
 authRoutes.post('/login', userLogin);
 authRoutes.get('/users', getAllUsers);
 authRoutes.post('/logout', userLogout);
